3d65a7ba842d593fd0042c12c7099bbfaf572c01
Gorbunoba Nina <gnina56565@gmail.com>
2026-01-05 20:53:02 +0400

    BCORE-3

--- src/main/java/ru/mentee/power/crm/domain/Lead.java
+++ src/main/java/ru/mentee/power/crm/domain/Lead.java
@@ -1,11 +1,25 @@
 package ru.mentee.power.crm.domain;
 
+import java.util.Objects;
+
 public class Lead {
     private String id;
     private String email;
     private String phone;
     private String company;
     private String status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Lead lead = (Lead) o;
+        return Objects.equals(id, lead.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 
     public Lead (String id, String email, String phone, String company, String status) {
         this.id = id;
