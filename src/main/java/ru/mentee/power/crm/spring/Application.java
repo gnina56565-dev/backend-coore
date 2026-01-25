960e85b43771ae28df2f3a4f522b0923b5412b8d
Gorbunoba Nina <gnina56565@gmail.com>
2026-01-25 14:59:07 +0400

    BCORE-15-2 FIX!!!!

--- src/main/java/ru/mentee/power/crm/spring/Application.java
+++ src/main/java/ru/mentee/power/crm/spring/Application.java
@@ -3,10 +3,13 @@ package ru.mentee.power.crm.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "ru.mentee.power.crm.spring.controller",  // ← LeadController!
+        "ru.mentee.power.crm.spring.service",     // ← LeadService!
+        "ru.mentee.power.crm.spring.repository"   // ← LeadRepository!
+})
 public class Application {
-
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
