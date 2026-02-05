cd40449a36b89090293ce752be2ff3fcd7465a10
Gorbunoba Nina <gnina56565@gmail.com>
2026-02-05 19:54:59 +0400

    BCORE-18

--- src/main/java/ru/mentee/power/crm/spring/Application.java
+++ src/main/java/ru/mentee/power/crm/spring/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "ru.mentee.power.crm")
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
