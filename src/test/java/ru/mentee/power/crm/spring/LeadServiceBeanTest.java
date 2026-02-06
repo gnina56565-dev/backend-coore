0eb4fd4ddf592df6b1a176f84a13c357b8567ae4
Gorbunoba Nina <gnina56565@gmail.com>
2026-02-07 03:21:46 +0400

    BCORE-19-new

--- src/test/java/ru/mentee/power/crm/spring/LeadServiceBeanTest.java
+++ src/test/java/ru/mentee/power/crm/spring/LeadServiceBeanTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LeadServiceBeanTest {
