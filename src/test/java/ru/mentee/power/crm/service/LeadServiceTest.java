6d02045c7b7a868d5dfe01e4d3d50768ebced31c
Gorbunoba Nina <gnina56565@gmail.com>
2026-03-08 14:47:29 +0400

    BCORE-27-CheckStyle

--- src/test/java/ru/mentee/power/crm/service/LeadServiceTest.java
+++ src/test/java/ru/mentee/power/crm/service/LeadServiceTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.mentee.power.crm.model.Lead;
 import ru.mentee.power.crm.model.LeadStatus;
-import ru.mentee.power.crm.spring.repository.InMemoryLeadRepository;
-import ru.mentee.power.crm.spring.repository.LeadRepository;
+import ru.mentee.power.crm.repository.LeadRepository;
 import ru.mentee.power.crm.spring.service.LeadService;
 
 import java.util.List;
@@ -21,7 +20,6 @@ class LeadServiceTest {
 
     @BeforeEach
     void setUp() {
-        repository = new InMemoryLeadRepository();
         service = new LeadService(repository);
     }
 
