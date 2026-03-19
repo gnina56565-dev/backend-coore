9d60e6904deffcc4c2ad3144de5996ac357ce752
Gorbunoba Nina <gnina56565@gmail.com>
2026-03-19 20:52:13 +0400

    BCORE-30-locking

--- src/test/java/ru/mentee/power/crm/spring/service/LeadLockingServiceTest.java
+++ src/test/java/ru/mentee/power/crm/spring/service/LeadLockingServiceTest.java
@@ -11,7 +11,12 @@ import ru.mentee.power.crm.repository.LeadJpaRepository;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
