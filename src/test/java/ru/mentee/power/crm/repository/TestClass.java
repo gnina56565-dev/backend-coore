0d668b914f8086c8bfac6e1ccef13d5b3d61a331
Gorbunoba Nina <gnina56565@gmail.com>
2026-04-24 02:17:33 +0400

    BCORE-34: Configure Spotless auto-formatting with checkstyle-12

--- src/test/java/ru/mentee/power/crm/repository/TestClass.java
+++ src/test/java/ru/mentee/power/crm/repository/TestClass.java
@@ -14,26 +14,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 class DatabaseConfigTest {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-    @Test
-    void shouldConnectToH2Database() {
-        String databaseProductName = jdbcTemplate
-                .execute((ConnectionCallback<String>) connection -> connection.getMetaData().getDatabaseProductName());
+  @Test
+  void shouldConnectToH2Database() {
+    String databaseProductName = jdbcTemplate
+        .execute((ConnectionCallback<String>) connection -> connection.getMetaData().getDatabaseProductName());
 
-        assertThat(databaseProductName).isEqualTo("H2");
-    }
+    assertThat(databaseProductName).isEqualTo("H2");
+  }
 
-    @Test
-    void shouldHaveLeadsTableCreated() {
-        boolean tableExists = jdbcTemplate.execute((ConnectionCallback<Boolean>) connection -> {
-            var resultSet = connection.getMetaData().getTables(null, null, "leads", new String[]{"TABLE"});
-            boolean exists = resultSet.next();
-            resultSet.close();
-            return exists;
-        });
+  @Test
+  void shouldHaveLeadsTableCreated() {
+    boolean tableExists = jdbcTemplate.execute((ConnectionCallback<Boolean>) connection -> {
+      var resultSet = connection.getMetaData().getTables(null, null, "leads", new String[]{"TABLE"});
+      boolean exists = resultSet.next();
+      resultSet.close();
+      return exists;
+    });
 
-        assertThat(tableExists).isTrue();
-    }
+    assertThat(tableExists).isTrue();
+  }
 }
