510931f20a895b50b8e857f3a26ffd572bea7582
Gorbunoba Nina <gnina56565@gmail.com>
2026-04-23 15:49:23 +0400

    BCORE-34: Configure Spotless auto-formatting with checkstyle-4

--- src/test/java/ru/mentee/power/crm/spring/repository/JpaLeadRepositoryTest.java
+++ src/test/java/ru/mentee/power/crm/spring/repository/JpaLeadRepositoryTest.java
@@ -3,7 +3,6 @@ package ru.mentee.power.crm.spring.repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,7 +20,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ComponentScan(basePackages = "ru.mentee.power.crm.spring.repository", includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 class JpaLeadRepositoryTest {
 
