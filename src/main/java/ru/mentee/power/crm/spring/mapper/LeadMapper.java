22b0899a37efc6ecd509da4039ab1f85f32ca01c
Gorbunoba Nina <gnina56565@gmail.com>
2026-06-09 01:34:33 +0400

    BCORE-40

--- src/main/java/ru/mentee/power/crm/spring/mapper/LeadMapper.java
+++ src/main/java/ru/mentee/power/crm/spring/mapper/LeadMapper.java
@@ -4,28 +4,31 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import ru.mentee.power.crm.model.Lead;
-import ru.mentee.power.crm.spring.dto.CreateLeadRequest;
-import ru.mentee.power.crm.spring.dto.LeadResponse;
-import ru.mentee.power.crm.spring.dto.UpdateLeadRequest;
+import ru.mentee.power.crm.spring.dto.generated.CreateLeadRequest;
+import ru.mentee.power.crm.spring.dto.generated.LeadResponse;
+import ru.mentee.power.crm.spring.dto.generated.UpdateLeadRequest;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DateTimeMapper.class})
 public interface LeadMapper {
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "createdAt", ignore = true)
-  @Mapping(target = "status", ignore = true)
-  @Mapping(target = "version", ignore = true)
-  @Mapping(target = "company", ignore = true)
-  @Mapping(target = "companyName", source = "company")
-  Lead toEntity(CreateLeadRequest dto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "companyName", source = "company")
+    Lead toEntity(CreateLeadRequest dto);
 
-  @Mapping(source = "company.name", target = "company")
-  LeadResponse toResponse(Lead entity);
+    @Mapping(source = "company.name", target = "company")
+    @Mapping(target = "firstName", ignore = true)
+    @Mapping(target = "lastName", ignore = true)
+    LeadResponse toResponse(Lead entity);
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "createdAt", ignore = true)
-  @Mapping(target = "status", ignore = true)
-  @Mapping(target = "version", ignore = true)
-  @Mapping(target = "company", ignore = true)
-  void updateEntity(UpdateLeadRequest dto, @MappingTarget Lead entity);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "companyName", ignore = true)
+    void updateEntity(UpdateLeadRequest dto, @MappingTarget Lead entity);
 }
